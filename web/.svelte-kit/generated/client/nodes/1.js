76797ac4ecd652667e498e2a64259bc0b95eb331
Nazif Barassounon <nazifbarassounon@gmail.com>
2025-03-24 21:41:48 +0100

    feat(search): implement the search result page

--- web/.svelte-kit/generated/client/nodes/1.js
+++
@@ -1,1 +0,0 @@
-export { default as component } from "../../../../node_modules/.pnpm/@sveltejs+kit@2.20.2_@sveltejs+vite-plugin-svelte@5.0.3_svelte@5.25.3_vite@6.2.2_@types_0fc65bc00d04238fb7bde953346c69d9/node_modules/@sveltejs/kit/src/runtime/components/svelte-5/error.svelte";
