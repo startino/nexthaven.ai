0022fe4831838f9e35c8ead8729d76e510252130
antopiahk <antopiahk@gmail.com>
2025-04-02 16:33:01 +0700

    fix(folder): added missing export

--- sveltekit/src/lib/components/folder/index.ts
+++ sveltekit/src/lib/components/folder/index.ts
@@ -1,5 +1,6 @@
 import CollectionList from './CollectionList.svelte';
 import AddToCollection from './AddToCollection.svelte';
 import CollectionProperties from './CollectionProperties.svelte';
+import FolderList from './FolderList.svelte';
 
-export { CollectionList, AddToCollection, CollectionProperties };
+export { CollectionList, AddToCollection, CollectionProperties, FolderList };
