e7deebd3aa2c1ef6d4d2536ee9c5fb8490a8c772
antopiahk <antopiahk@gmail.com>
2025-04-02 17:25:03 +0700

    fix(collections): removing old files

--- sveltekit/src/lib/components/folder/index.ts
+++ sveltekit/src/lib/components/folder/index.ts
@@ -1,6 +1,5 @@
 import CollectionList from './CollectionList.svelte';
 import AddToCollection from './AddToCollection.svelte';
 import CollectionProperties from './CollectionProperties.svelte';
-import FolderList from './FolderList.svelte';
 
-export { CollectionList, AddToCollection, CollectionProperties, FolderList };
+export { CollectionList, AddToCollection, CollectionProperties };
--- sveltekit/src/routes/folders/+page.ts
+++
@@ -1,7 +0,0 @@
-import { redirect } from '@sveltejs/kit';
-import type { PageLoad } from './$types';
-
-export const load: PageLoad = async ({ parent }) => {
-	// Redirect to the collections route
-	redirect(301, '/collections');
-};
