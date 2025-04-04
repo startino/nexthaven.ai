cd4593ddcce12a8bd6dd6c89d98cb8151dd27110
JalilUmar <jalil786.umer@gmail.com>
2025-04-04 18:35:14 +0500

    fix(evaluation): add llm to calculate nightly rates from booking.com

--- services/api/src/models/apify.py
+++ services/api/src/models/apify.py
@@ -29,43 +29,57 @@ APIFY_MAX_ITEMS = int(os.getenv("APIFY_MAX_ITEMS", 10))
 #     max_price: int = Field(description="The maximum price to search for")
 #     includes: ApifyInclude = Field(default=ApifyInclude(), description="The includes to search for")
 
+
 class BookingApifyRequest(BaseModel):
     search: str = Field(description="The search query")
-    maxItems: int = Field(default=APIFY_MAX_ITEMS, description="The maximum number of items to return")
-    # propertyType: Literal["Apartments", "Hotels", "Hostels", "Guest houses", 
-                          # "Homestays", "Bed and breakfasts", "Holiday homes", 
-                          # "Boats","Villas", "Motels", "Resorts", "Holiday parks", 
-                          # "Campsites", "Luxury tents"] = Field(description="The type of property to search for")
+    maxItems: int = Field(
+        default=APIFY_MAX_ITEMS, description="The maximum number of items to return"
+    )
+    # propertyType: Literal["Apartments", "Hotels", "Hostels", "Guest houses",
+    # "Homestays", "Bed and breakfasts", "Holiday homes",
+    # "Boats","Villas", "Motels", "Resorts", "Holiday parks",
+    # "Campsites", "Luxury tents"] = Field(description="The type of property to search for")
     sortBy: str = Field(default="review_score_and_price", description="The sort order")
     starsCountFilter: str = Field(default="any", description="The stars count filter")
-    currency: str = Field(default="USD", description="The currency to use for the search")
-    language: str = Field(default="en-gb", description="The language to use for the search")
+    currency: str = Field(
+        default="USD", description="The currency to use for the search"
+    )
+    language: str = Field(
+        default="en-gb", description="The language to use for the search"
+    )
     rooms: int = Field(default=1, description="The number of rooms to search for")
     adults: int = Field(default=2, description="The number of adults to search for")
     children: int = Field(default=0, description="The number of children to search for")
-    minMaxPrice: str = Field(default="0-2000", description="The minimum and maximum price to search for")
+    minMaxPrice: str = Field(
+        default="0-2000", description="The minimum and maximum price to search for"
+    )
     checkIn: str = Field(description="The check-in date")
     checkOut: str = Field(description="The check-out date")
-    
+
+
 class Location(BaseModel):
     lat: float
     lng: float
-    
+
+
 class Address(BaseModel):
     full: str | None
     postalCode: str | None
     street: str | None
     country: str | None
     region: str | None
-    
+
+
 class Details(BaseModel):
     name: str | None
     additionalInfo: list[str] | None
-    
+
+
 class Facilities(BaseModel):
     name: str | None
     overview: str | None
     facilities: list[Details]
+
 
 class Room(BaseModel):
     available: bool | None
@@ -74,11 +88,13 @@ class Room(BaseModel):
     persons: int | None
     bedTypes: list[Details] | None
     facilities: list[str] | None
-    
+
+
 class CategoryReview(BaseModel):
     title: str | None
     score: float | None
-    
+
+
 class BookingApifyResponse(BaseModel):
     url: str
     name: str
